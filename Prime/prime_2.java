66a6d0ed01a978c56f7d15ce59e16307752c34d1
Calista Huang <calistahuang@calistas-air.attlocal.net>
2020-07-12 16:49:08 -0700

    Part 2: using multiplication

--- Prime/prime_2.java
+++ Prime/prime_2.java
@@ -4,12 +4,15 @@ public class prime_2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
         int number = scan.nextInt();
+        // Create an array storing all number values from zero to the number
         int[] array = new int[number + 1];
+        // Mark all multiples of i starting from 2 until half the number.
         for (int i = 2; i < number; i++) {
             for (int j = 1; j < number / 2; j++) {
                 array[i * j] = 1;
             }
         }
+        // If number isn't marked, it's prime.
         if (array[number + 1] == 0) {
             System.out.println("Prime!");
         }
